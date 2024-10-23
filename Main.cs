284f7c734b8d80450b5d36942e8c7a05f9a99b0d
thehambeard <61787444+thehambeard@users.noreply.github.com>
2024-10-23 05:49:31 -0400

    Fix Hotkey suppression and migrate to CinematicUnityExplorer

--- Main.cs
+++ Main.cs
@@ -1,129 +1,19 @@
 ﻿using HarmonyLib;
-
-using Kingmaker;
 using Kingmaker.UI;
 using Kingmaker.UI.MVVM;
-using Kingmaker.UI.Selection;
-
-using MicroUtils.Transpiler;
-
-using Owlcat.Runtime.Core.Logging;
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-
 using UnityExplorer;
-using UnityExplorer.UI;
 
 using UnityModManagerNet;
 
 namespace UnityExplorerLoader
 {
-    [HarmonyPatch]
-    class InputFocusHandler : MonoBehaviour
-    {
-        //public InputField component;
-        //bool keyboardWasDisabled;
-
-        //void Update()
-        //{
-        //    if (component.isFocused)
-        //    {
-        //        if (!Game.Instance.Keyboard.Disabled)
-        //        {
-        //            //#if DEBUG
-        //            //                    Main.Logger.Log("Disabling keyboard");
-        //            //#endif
-        //            keyboardWasDisabled = true;
-        //            Game.Instance.Keyboard.Disabled.SetValue(true);
-        //        }
-        //        return;
-        //    }
-
-        //    if (Game.Instance.Keyboard.Disabled && !component.isFocused && keyboardWasDisabled)
-        //    {
-        //        //#if DEBUG
-        //        //                Main.Logger.Log("Enabling keyboard");
-        //        //#endif
-        //        keyboardWasDisabled = false;
-        //        Game.Instance.Keyboard.Disabled.SetValue(false);
-        //    }
-        //}
-
-        [HarmonyPatch(typeof(UIFactory), nameof(UIFactory.CreateInputField))]
-        [HarmonyPostfix]
-        static InputFieldRef UIFactory_CreateInputField_Postfix(InputFieldRef __result)
-        {
-            __result.Component.gameObject.AddComponent<InputFocusHandler>()/*.component = __return.Component*/;
-
-            return __result;
-        }
-    }
-
-    [HarmonyPatch(typeof(KeyboardAccess))]
-    static class KeyboardAccessPatch
-    {
-        //[HarmonyPatch(nameof(KeyboardAccess.IsInputFieldSelected))]
-        //[HarmonyPrefix]
-        //static bool Prefix(ref bool __result)
-        //{
-        //    try
-        //    {
-        //        EventSystem current = EventSystem.current;
-                
-        //        if (current == null)
-        //            return true;
-
-        //        GameObject selectedGameObject = current.currentSelectedGameObject;
-        //        if (selectedGameObject != null && selectedGameObject.GetComponent<InputFocusHandler>() != null)
-        //        {
-        //            __result = true;
-        //            return false;
-        //        }
-
-        //        return true;
-        //    }
-        //    finally
-        //    {
-        //    }
-        //}
-
-        [HarmonyPatch(nameof(KeyboardAccess.IsInputFieldSelected))]
-        [HarmonyPostfix]
-        static bool Postfix(bool __result)
-        {
-            try
-            {
-                EventSystem current = EventSystem.current;
-
-                if (current == null)
-                    return __result;
-
-                GameObject selectedGameObject = current.currentSelectedGameObject;
-                if (selectedGameObject != null && selectedGameObject.GetComponent<InputField>() != null)
-                {
-                    return true;
-                }
-
-                return __result;
-            }
-            finally
-            {
-            }
-        }
-    }
-
     [HarmonyPatch]
     class UnityExplorerLoader
     {
@@ -186,13 +76,6 @@ namespace UnityExplorerLoader
             modEntry.OnUnload = OnUnload;
             modEntry.OnGUI = OnGUI;
             HarmonyInstance = new Harmony(modEntry.Info.Id);
-            
-            var uePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "UnityExplorer.STANDALONE.Mono.dll");
-            
-            Logger.Log($"Loading Unity Explorer from {uePath}");
-
-            Assembly.LoadFrom(uePath);
-
             HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
             return true;
         }
@@ -205,46 +88,19 @@ namespace UnityExplorerLoader
             return true;
         }
     }
+
+    [HarmonyPatch]
+    static class KeyboardAccessPatch
+    {
+        [HarmonyPatch(typeof(KeyboardAccess), nameof(KeyboardAccess.IsInputFieldSelected))]
+        [HarmonyPostfix]
+        static void CheckFieldType(ref bool __result)
+        {
+            if (__result == true) return;
+
+            __result = EventSystem.current != null 
+                && EventSystem.current.currentSelectedGameObject != null
+                && EventSystem.current.currentSelectedGameObject.gameObject.GetComponent<InputField>() != null;
+        }
+    }
 }
-
-//[HarmonyPatch]
-//static class AnnoyingMessagesPatch
-//{
-//    static MethodBase TargetMethod() =>
-//    typeof(KingmakerInputModule)
-//        .GetNestedTypes(AccessTools.all)
-//        .Where(t => t.GetCustomAttributes<CompilerGeneratedAttribute>().Any())
-//        .Select(t => t.GetMethod("MoveNext", AccessTools.all))
-//        .FirstOrDefault();
-
-//    [HarmonyTranspiler]
-//    static IEnumerable<CodeInstruction> CheckEventSystem_Transpiler(IEnumerable<CodeInstruction> instructions)
-//    {
-//        var matchPattern = new Func<CodeInstruction, bool>[]
-//        {
-//            ci => ci.opcode == OpCodes.Ldsfld,
-//            ci => ci.opcode == OpCodes.Ldstr,
-//            ci => ci.Calls(AccessTools.Method(typeof(LogChannel), nameof(LogChannel.Log), [typeof(string)]))
-//        };
-
-//        bool replaceWithNop(IEnumerable<CodeInstruction> instructions)
-//        {
-//            var match = instructions.FindInstructionsIndexed(matchPattern);
-
-//            if (match.Count() != 3)
-//                return false;
-
-//            foreach (var (_, instruction) in match)
-//            {
-//                instruction.opcode = OpCodes.Nop;
-//                instruction.operand = null;
-//            }
-
-//            return true;
-//        }
-
-//        while (replaceWithNop(instructions)) { }
-
-//        return instructions;
-//    }
-//}
